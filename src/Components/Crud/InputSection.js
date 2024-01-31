06cd335e13e16ce944b16e748c401f9e89a3a15c
Aniket <aniketkule231997@gmail.com>
2024-01-31 18:28:18 +0530

    kkc

--- src/Components/Crud/InputSection.js
+++ src/Components/Crud/InputSection.js
@@ -4,6 +4,7 @@ import './style.css';
 function InputSection() {
   const [value, setValue] = useState('');
   const [newtodo, setNewtodo] = useState([]);
+  const [currentLanguage, setCurrentLanguage] = useState('en'); // Default language is 'en'
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -30,7 +31,7 @@ function InputSection() {
   };
 
   const handleUpdate = (index) => {
-    const updatedTodo = prompt(translations.update.en, newtodo[index]);
+    const updatedTodo = prompt(translations.update[currentLanguage], newtodo[index]);
     if (updatedTodo !== null) {
       const updatedTodos = [...newtodo];
       updatedTodos[index] = updatedTodo;
@@ -39,7 +40,7 @@ function InputSection() {
   };
 
   const handleDelete = (index) => {
-    const confirmDelete = window.confirm(translations.delete.en);
+    const confirmDelete = window.confirm(translations.delete[currentLanguage]);
     if (confirmDelete) {
       const updatedTodos = [...newtodo];
       updatedTodos.splice(index, 1);
@@ -47,9 +48,20 @@ function InputSection() {
     }
   };
 
+  const handleLanguageChange = (language) => {
+    setCurrentLanguage(language);
+  };
+
   return (
     <div className="inputContainer">
       <h1>CRUD Web App</h1>
+      <div className='languagecontainer'>
+        <select value={currentLanguage} onChange={(e) => handleLanguageChange(e.target.value)}>
+          <option value="en">English</option>
+          <option value="es">Español</option>
+          <option value="fr">Français</option>
+        </select>
+      </div>
       <div className="inputdiv">
         <input
           type="text"
@@ -58,7 +70,7 @@ function InputSection() {
           onChange={(e) => setValue(e.target.value)}
         />
         <button className="addBtn" onClick={handleChange}>
-          {translations.add.en}
+          {translations.add[currentLanguage]}
         </button>
       </div>
 
@@ -67,10 +79,10 @@ function InputSection() {
           <div className="todoItem" key={i}>
             <p className="itemtag">{item}</p>
             <button className="updatebtn" onClick={() => handleUpdate(i)}>
-              {translations.update.en}
+              {translations.update[currentLanguage]}
             </button>
             <button className="deletebtn" onClick={() => handleDelete(i)}>
-              {translations.delete.en}
+              {translations.delete[currentLanguage]}
             </button>
           </div>
         ))}
